9bddf9676b9fb9c6be8e6d802e64763280b19f29
Ville Komulainen <ville.m.komulainen@utu.fi>
2022-04-27 03:15:26 +0300

    uusia testeja

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,9 +1,13 @@
 import unittest
 from main.src.scraper import scrape_xml_yle
 
-def test_htppRequest():
+def test_ylescrapet():
     artikkelit = scrape_xml_yle()
     #Varmista,että artikkeleja on imuroitu
     assert len(artikkelit) > 0
-    
+    #Varmista, että palautettu tyyppi on lista, jotta jsoniksi muuttaminen onnistuu
+    assert type(artikkelit) == list
+    #Katso, että kaikkiin on tallentunut kaikki tiedot: Otsikko, julkaisupvm, teksti
+    for art in artikkelit:
+        assert len(art) == 3
 
